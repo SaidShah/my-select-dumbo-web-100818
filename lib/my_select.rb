5334a1f55c002a5918524a211ec5c9726ceaf16e
saidmysr20@gmail.com <saidmysr20@gmail.com>
2018-08-16 19:57:53 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,11 +1,11 @@
 def my_select(collection)
- if block_given?
+
  counter = 0 
  even_nums = []
  while counter < collection.length 
-   even_nums << yield(collection[counter].even?)
+   if collection[counter].even?
     counter+=1 
   end
- end
+
  even_nums
 end
